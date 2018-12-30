d14d2e523def1c32395a2974eaf89537305be24b
HuseyinOrkun <huseyinorkunelmas@gmail.com>
2018-12-30 18:09:06 +0300

    fixes some bugs

--- IEproject.py
+++ IEproject.py
@@ -2,14 +2,18 @@ import gurobi as gb
 import numpy as np
 from gurobipy import *
 
+
+# Check for symmetry afterwards
 # Problem data
 
-costs = [[None,100,140,115,130,140,135],
+costs = [
+		[None,100,140,115,130,140,135],
         [None,None,105,140,110,125,130],
         [None,None,None,130,145,160,150],
         [None,None,None,None,135,150,105],
         [None,None,None,None,None,120,165],
-        [None,None,None,None,None,None,130]]
+        [None,None,None,None,None,None,130]
+        ]
 
 # Revenues for the first year rev[[t][i][j]]
 revt =  [[[None,30,20,35,25,18,30],
@@ -18,7 +22,7 @@ revt =  [[[None,30,20,35,25,18,30],
         [None,None,None,None,35,25,38],
         [None,None,None,None,None,30,40],
         [None,None,None,None,None,None,30]],# Revenues for the second year
-         [[None,24,16,28,20,15,24],
+        [[None,24,16,28,20,15,24],
         [None,None,23,18,20,28,36],
         [None,None,None,25,28,26,32],
         [None,None,None,None,28,20,31],
@@ -38,67 +42,52 @@ model = gb.Model('City Linking')
 model.modelSense = GRB.MAXIMIZE
 
 
-# Connectivity Variable
+# Connectivity Variable nodes are connected at the end of year t.
 y_t = {}
 
-
 for t in T:
-    for i in range(n):
+    for i in range(n-1):    
         for j in range(i+1,n):
-            y_t[t-1,i,j] = model.addVar(obj= (revt[t-1][i][j]-costs[i][j]), name='connected in year '+ str(t)+ ' between ' +str(i)+' and '+str(j))
-            y_t[t-1,j,i] = y_t[t-1,i,j] 
-            y_t[t-1,i,i] = model.addVar(obj= (revt[t-1][i][j]-costs[i][j]), name='connected in year '+ str(t)+ ' between ' +str(i)+' and '+str(i))
-y_t[0,6,6] =  model.addVar(obj= (revt[0][5][6]-costs[5][6]), name='connected in year '+ str(1)+ ' between ' +str(6)+' and '+str(6))
-y_t[1,6,6] =  model.addVar(obj= (revt[1][5][6]-costs[5][6]), name='connected in year '+ str(2)+ ' between ' +str(6)+' and '+str(6))
+            y_t[t-1,i,j] = model.addVar(obj= (revt[t-1][i][j]), name='conn. in y'+ str(t)+ ' between ' +str(i)+' and '+str(j))
 
-
-# Linked Variable
+# Linked Variable, built in year t
 x = {}
 for t in T:    
-    for i in range(n):
+    for i in range(n-1):
         for j in range(i+1,n):
-            if(t == 2):
-                cost = costs[i][j]
-            else:
-                cost = 0
-            x[t-1,i,j] = model.addVar(obj= (-cost), name='Linked in year '+ str(t)+ ' between ' +str(i)+' and '+str(j))
-            x[t-1,j,i] = x[t-1,i,j] 
-            x[t-1,i,i] = model.addVar(obj= (-cost), name='Linked in year '+ str(t)+ ' between ' +str(i)+' and '+str(i))
-x[0,6,6] =  model.addVar(obj= (0), name='Linked in year '+ str(1)+ ' between ' +str(6)+' and '+str(6))
-x[1,6,6] =  model.addVar(obj= (-costs[5][6]), name='Linked in year '+ str(2)+ ' between ' +str(6)+' and '+str(6))
+            x[t-1,i,j] = model.addVar(obj= (-costs[i][j]), name='Linked in year '+ str(t)+ ' between ' +str(i)+' and '+str(j))
 
 model.update()
 
 # Constraints
 for i in range(n):
-    for j in range(n):
-        model.addConstr(x[0,i,j] <= x[1,i,j], "Existent link continuity")
+    for j in range(i+1, n):
+            print("i: ", x[0,i,j], " - j: ", x[1,i,j])
+            model.addConstr(x[0,i,j] + x[1,i,j] <= 1, "A link can not be built in 2 years")
 
-model.addConstrs((y_t[1,i,j] == 1 for i in range(n)
-                              for j in range(n)
-                              if i != j), name='Connectivity at the end')
-    
-model.addConstr(2+20*quicksum((y_t[0,i,j] for i in range(n) for j in range(n)))+49<= 0, "Ring network 1")
-model.addConstr(20*quicksum(y_t[0,i,j] for i in range(n) for j in range(n)) <= 0, 'Ring network 2')
+
+model.addConstrs((y_t[1,i,j] + y_t[0,i,j] >= 1 for i in range(n)
+                                          for j in range(i+1, n)), name='Connectivity at the end')
+   
+
+# Check ring network constraints
+model.addConstr(2 - 20*(49 - quicksum(y_t[0,i,j] for i in range(n) for j in range(i+1, n))) <= 0, "Ring network 1")
+model.addConstr(20*quicksum(y_t[0,i,j] for i in range(n) for j in range(i+1, n)) <= 0, 'Ring network 2')
 
 
 for i in range(n):
-    for j in range(n):
-        model.addConstrs(x[t-1,i,j] <= y_t[t-1,i,j] for t in T)
+    for j in range(i+1, n):
+        model.addConstrs((x[t-1,i,j] <= y_t[t-1,i,j] for t in T), 'link implies Connectivity')
 
-model.addConstrs(y_t[t-1,i,i] == 1 for i  in range(n) for t in T)
-model.addConstrs(x[t-1,i,i] == 1 for i  in range(n) for t in T)
-
-model.addConstr(20*quicksum(y_t[0,i,j] for i in range(n) for j in range(n)) <= 0, 'Ring network 2')
 for i in range(n):
-    for j in range(n):
-        for k in range(n):
-            model.addConstrs(y_t[t-1,i,j] + y_t[t-1,j,k] - 1 <= y_t [t-1,i,k] for t in T
+    for j in range(i+1, n):
+        for k in range(j+1, n):
+            model.addConstrs((y_t[t-1,i,j] + y_t[t-1,j,k] - 1 <= y_t [t-1,i,k] for t in T), 'connector')
 
 model.update()
+model.write("file.lp")
 
 # Objective
-model
 
 # Optimization
 model.optimize()
